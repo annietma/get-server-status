364ac296ae33ea15f89c9d045e114a53d1848b95
annietma <anniema2020@gmail.com>
2024-04-06 17:17:26 -0700

    basic UX usage of library

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,34 @@
-import { getTranslationStatus } from "./lib/utils";
+"use client";
+import { useEffect, useState } from "react";
+import { useStatus } from "./lib/clientLibrary";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-export default async function Home() {
-  await getTranslationStatus((Math.random() * 1000).toString());
-  return null;
+export default function Home() {
+  const { statusLogs, subscribeToStatus } = useStatus();
+  const [jobId, setJobId] = useState(
+    Math.floor(Math.random() * 10000).toString()
+  );
+
+  return (
+    <div className="bg-white h-screen w-screen flex flex-col items-center pt-20">
+      <div className="w-1/2 items-center flex flex-col gap-4">
+        <div className="flex flex-row items-center gap-2 w-full">
+          <Input
+            placeholder="Enter Job ID"
+            value={jobId}
+            onChange={(e) => setJobId(e.target.value)}
+          />
+          <Button onClick={() => subscribeToStatus(jobId)}>Submit</Button>
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          {statusLogs.map((log, i) => (
+            <div key={i} className="">
+              {log.timestamp.toLocaleTimeString()}: {log.jobId} - {log.status}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
